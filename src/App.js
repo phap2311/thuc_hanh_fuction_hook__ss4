ee0cc0921d172b11696fbafe2062b83810bc99c3
dangphapqt1997 <dangphapqt1997@gmail.com>
2024-04-03 16:58:19 +0700

    thuc-hanh-fuction-hook-ss4

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Counter from "./components/Counter";
+import Selector from "./components/Selector";
+import MyClock from "./components/MyClock";
+
+function App() {
+    return (
+        // <Counter/>
+        <Selector/>
+       // <MyClock/>
+    );
+}
+
+export default App;
---
+++ src/components/MyClock.js
@@ -0,0 +1,13 @@
+import useClock from "./useClock";
+
+export default function MyClock() {
+    const [time, ampm] = useClock();
+    return (
+        <>
+            <div id="Clock-style">
+                {time}
+                <span>{ampm}</span>
+            </div>
+        </>
+    )
+}
